d77d29f6fa07d462c148b5dd9f0e353b71de5330
Silas Selekane <s.selekane@gmail.com>
2025-04-11 10:55:32 +0200

    models and migrations added

---
+++ database/migrations/2025_04_11_085359_create_performance_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('performance_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('comments');
+            $table->integer('rating')->comment('1 to 5');
+            $table->date('review_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('performance_reviews');
+    }
+};
